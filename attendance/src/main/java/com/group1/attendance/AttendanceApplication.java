4845cd74d891f098759b36cc4cfdab1bdc3c7663
uxr070301 <uxr070301@darden.com>
2023-12-02 11:53:57 +0530

    Group1

---
+++ attendance/src/main/java/com/group1/attendance/AttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.group1.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceApplication.class, args);
+	}
+
+}
---
+++ attendance/src/test/java/com/group1/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
